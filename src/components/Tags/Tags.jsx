40db8a54d5590dba679ee4b7fab07f1321e4abc5
Maryangela Jaimes <maryangelajc.6@gmail.com>
2024-03-25 00:00:57 -0300

    elimine import css tags

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,12 +1,11 @@
-import './Tags.css'
-import Badge from 'react-bootstrap/Badge';
+import Badge from "react-bootstrap/Badge";
 
-const Tags=({raza, bgColor})=>{
+const Tags = ({ raza, bgColor }) => {
   return (
     <div>
-        <Badge bg={bgColor}>{raza}</Badge>
+      <Badge bg={bgColor}>{raza}</Badge>
     </div>
   );
-}
+};
 
 export default Tags;
